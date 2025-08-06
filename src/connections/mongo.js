7439694c097a143744381f03367b0e526a583928
Roccoadn <roccoandreone@gmail.com>
2025-08-06 12:13:01 -0300

    mas pruebas

--- src/connections/mongo.js
+++ src/connections/mongo.js
@@ -6,11 +6,11 @@ const mongoConnection = async () => {
     try{
         await mongoose.connect(process.env.MONGO_KEY, { dbName: 'MercaditoDB' });
         console.log('Conexion exitosa a la base de datos');
-        console.log("Mongo URI en producción:", process.env.MONGO_KEY);
     }
     catch{
         console.log('Error al conectar a la base de datos');
     }
+    console.log("Mongo URI en producción:", process.env.MONGO_KEY);
 }
 
 export default mongoConnection;
